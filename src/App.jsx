7ccf4984d8c172a6ee933ac2d7c2117a8aba1737
samjoel123 <samjojeorge05@gmail.com>
2026-05-28 01:45:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import ProductList from "./components/ProductList";
 
 function App() {
+
+  const [showProducts, setShowProducts] = useState(false);
+
   return (
     <div className="background-image">
-      <div className="content">
-        <h1>Welcome to Paradise Nursery</h1>
 
-        <p>
-          Paradise Nursery brings beautiful plants to your home.
-        </p>
+      {!showProducts ? (
 
-        <button>Get Started</button>
-      </div>
+        <div className="content">
+          <h1>Welcome to Paradise Nursery</h1>
+
+          <p>
+            Paradise Nursery brings beautiful plants to your home.
+          </p>
+
+          <button
+            onClick={() => setShowProducts(true)}
+          >
+            Get Started
+          </button>
+        </div>
+
+      ) : (
+
+        <ProductList />
+
+      )}
+
     </div>
   );
 }
