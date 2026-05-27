3f8c6617273ea4944e62e04b06ad5f99ce02a3be
samjoel123 <samjojeorge05@gmail.com>
2026-05-28 01:15:09 +0530

    Create App component with welcome message and button

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="background-image">
+      <h1>Welcome to Paradise Nursery</h1>
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
