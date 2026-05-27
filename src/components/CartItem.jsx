dfb8a8d6cd0a98c5b8d0d7f8216e4173e04d807a
samjoel123 <samjojeorge05@gmail.com>
2026-05-28 01:18:53 +0530

    Add CartItem component for shopping cart

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,13 @@
+function CartItem() {
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
+      <p>Plant Item</p>
+      <button>+</button>
+      <button>-</button>
+      <button>Delete</button>
+    </div>
+  );
+}
+
+export default CartItem;
