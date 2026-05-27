f873addb4f8fc0a5699917dab34ca978a71f161b
samjoel123 <samjojeorge05@gmail.com>
2026-05-28 01:37:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import './App.css';
+
 function App() {
   return (
     <div className="background-image">
-      <h1>Welcome to Paradise Nursery</h1>
-      <button>Get Started</button>
+      <div className="content">
+        <h1>Welcome to Paradise Nursery</h1>
+
+        <p>
+          Paradise Nursery brings beautiful plants to your home.
+        </p>
+
+        <button>Get Started</button>
+      </div>
     </div>
   );
 }
