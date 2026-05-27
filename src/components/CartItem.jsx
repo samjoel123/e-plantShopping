1e19772c6f388b46a920987190493dac48c70362
samjoel123 <samjojeorge05@gmail.com>
2026-05-28 01:40:52 +0530

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,11 +1,34 @@
 function CartItem() {
+
+  const item = {
+    name: "Aloe Vera",
+    price: "$10",
+    quantity: 1
+  };
+
   return (
     <div>
-      <h2>Shopping Cart</h2>
-      <p>Plant Item</p>
-      <button>+</button>
-      <button>-</button>
-      <button>Delete</button>
+      <h1>Shopping Cart</h1>
+
+      <div>
+        <h3>{item.name}</h3>
+
+        <p>{item.price}</p>
+
+        <p>Quantity: {item.quantity}</p>
+
+        <button>+</button>
+
+        <button>-</button>
+
+        <button>Delete</button>
+      </div>
+
+      <h2>Total Cart Amount: $10</h2>
+
+      <button>Continue Shopping</button>
+
+      <button>Checkout</button>
     </div>
   );
 }
